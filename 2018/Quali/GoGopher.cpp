e8a8a0e0e69740db0c0e24c36c2734f70c808c01
Kai-Niklas Bastian <kai-niklas.bastian@sap.com>
2018-04-18 10:05:49 +0200

    Replace tabs with spaces

--- 2018/Quali/GoGopher.cpp
+++ 2018/Quali/GoGopher.cpp
@@ -12,31 +12,31 @@ constexpr unsigned edgeLength = 1000u;
 
 struct Rectangle
 {
-	unsigned calcArea() const { return width * height; }
-	unsigned width;
-	unsigned height;
+    unsigned calcArea() const { return width * height; }
+    unsigned width;
+    unsigned height;
 };
 
 Rectangle findBestRectangle(unsigned minArea)
 {
-	constexpr unsigned minEdgeLength = 3u;
-	constexpr unsigned maxEdgeLength = edgeLength;
-	Rectangle bestRectangle = {1000u, 1000u};
+    constexpr unsigned minEdgeLength = 3u;
+    constexpr unsigned maxEdgeLength = edgeLength;
+    Rectangle bestRectangle = {1000u, 1000u};
 
-	for (unsigned i = minEdgeLength; i < maxEdgeLength; ++i)
-	{
-		for (unsigned j = minEdgeLength; j < maxEdgeLength; ++j)
-		{
-			Rectangle suggestion = {i, j};
-			if (suggestion.calcArea() >= minArea &&
-				suggestion.calcArea() < bestRectangle.calcArea())
-			{
-				bestRectangle = suggestion;
-			}
-		}
-	}
+    for (unsigned i = minEdgeLength; i < maxEdgeLength; ++i)
+    {
+        for (unsigned j = minEdgeLength; j < maxEdgeLength; ++j)
+        {
+            Rectangle suggestion = {i, j};
+            if (suggestion.calcArea() >= minArea &&
+                suggestion.calcArea() < bestRectangle.calcArea())
+            {
+                bestRectangle = suggestion;
+            }
+        }
+    }
 
-	return bestRectangle;
+    return bestRectangle;
 }
 
 template <typename T>
@@ -70,125 +70,125 @@ using Field = pair<unsigned, unsigned>;
 class FieldSuggester
 {
 public:
-	FieldSuggester(const Rectangle& t, const Matrix2<bool>& garden)
-		: target_(t)
-		, garden_(garden)
-	{
-		unsigned x = 2;
-		for (unsigned i = 0; i < target_.width; ++i)
-		{
-			unsigned y = 2;
-			for (unsigned j = 0; j < target_.height; ++j)
-			{
-				openFields_.push(make_pair(9, make_pair(x + i, y + j)));
-			}
-		}
-	}
+    FieldSuggester(const Rectangle& t, const Matrix2<bool>& garden)
+        : target_(t)
+        , garden_(garden)
+    {
+        unsigned x = 2;
+        for (unsigned i = 0; i < target_.width; ++i)
+        {
+            unsigned y = 2;
+            for (unsigned j = 0; j < target_.height; ++j)
+            {
+                openFields_.push(make_pair(9, make_pair(x + i, y + j)));
+            }
+        }
+    }
 
-	Field suggest()
-	{
-		while (true)
-		{
-			auto top = openFields_.top();
-			unsigned fieldValue = calcFieldValue(top.second);
-			if (top.first != fieldValue)
-			{
-				openFields_.pop();
-				openFields_.push(make_pair(fieldValue, top.second));
-				continue;
-			}
-			return top.second;
-		}
-	}
-
-private:
-	unsigned calcFieldValue(Field f) const
-	{
-		unsigned fields = 0;
-		for (unsigned i = f.first - 1; i <= f.first + 1; ++i)
-		{
-			for (unsigned j = f.second - 1; j <= f.second + 1; ++j)
-			{
-				if (!garden_.at(i,j))
-				{
-					++fields;
-				}
-			}
-		}
-		return fields;
-	}
+    Field suggest()
+    {
+        while (true)
+        {
+            auto top = openFields_.top();
+            unsigned fieldValue = calcFieldValue(top.second);
+            if (top.first != fieldValue)
+            {
+                openFields_.pop();
+                openFields_.push(make_pair(fieldValue, top.second));
+                continue;
+            }
+            return top.second;
+        }
+    }
 
 private:
-	Rectangle target_;
-	const Matrix2<bool>& garden_;
-	using queueT = pair<unsigned, Field>;
-	priority_queue<queueT> openFields_;
+    unsigned calcFieldValue(Field f) const
+    {
+        unsigned fields = 0;
+        for (unsigned i = f.first - 1; i <= f.first + 1; ++i)
+        {
+            for (unsigned j = f.second - 1; j <= f.second + 1; ++j)
+            {
+                if (!garden_.at(i,j))
+                {
+                    ++fields;
+                }
+            }
+        }
+        return fields;
+    }
+
+private:
+    Rectangle target_;
+    const Matrix2<bool>& garden_;
+    using queueT = pair<unsigned, Field>;
+    priority_queue<queueT> openFields_;
 };
 
 void communicate(Field f)
 {
-	cout << f.first + 1 << ' ' << f.second + 1 << '\n';
+    cout << f.first + 1 << ' ' << f.second + 1 << '\n';
 }
 
 pair<int, Field> getResponse()
 {
-	int a,b;
-	cin >> a >> b;
-	pair<int, Field> ret;
-	if (a == -1)
-	{
-		ret.first = -1;
-		return ret;
-	}
-	if (a == 0)
-	{
-		ret.first = 0;
-		return ret;
-	}
-	ret.first = 1;
-	ret.second.first = a - 1;
-	ret.second.second = b - 1;
-	return ret;
+    int a,b;
+    cin >> a >> b;
+    pair<int, Field> ret;
+    if (a == -1)
+    {
+        ret.first = -1;
+        return ret;
+    }
+    if (a == 0)
+    {
+        ret.first = 0;
+        return ret;
+    }
+    ret.first = 1;
+    ret.second.first = a - 1;
+    ret.second.second = b - 1;
+    return ret;
 }
 
 int solve()
 {
-	unsigned targetArea;
-	cin >> targetArea;
-	Matrix2<bool> garden(edgeLength, false);
-	const Rectangle targetRect = findBestRectangle(targetArea);
-	FieldSuggester suggester(targetRect, garden);
+    unsigned targetArea;
+    cin >> targetArea;
+    Matrix2<bool> garden(edgeLength, false);
+    const Rectangle targetRect = findBestRectangle(targetArea);
+    FieldSuggester suggester(targetRect, garden);
 
-	while (true)
-	{
-		Field nextRequest = suggester.suggest();
-		communicate(nextRequest);
-		pair<int, Field> response = getResponse();
-		if (response.first == -1)
-			return 1;
-		if (response.first == 0)
-			break;
-		Field resultingField = response.second;
+    while (true)
+    {
+        Field nextRequest = suggester.suggest();
+        communicate(nextRequest);
+        pair<int, Field> response = getResponse();
+        if (response.first == -1)
+            return 1;
+        if (response.first == 0)
+            break;
+        Field resultingField = response.second;
 
-		const bool wasProcessed =
-			garden.at(resultingField.first, resultingField.second);
-		if (wasProcessed)
-			continue;
+        const bool wasProcessed =
+            garden.at(resultingField.first, resultingField.second);
+        if (wasProcessed)
+            continue;
 
-		garden.set(resultingField.first, resultingField.second);
-	}
+        garden.set(resultingField.first, resultingField.second);
+    }
 
-	return 0;
+    return 0;
 }
 
 int main()
 {
-	uint_fast64_t t;
-	cin >> t;
-	for (uint_fast64_t tNumber = 1u; tNumber < t + 1; ++tNumber)
-	{
-		if(solve() == -1)
-			return -1;
-	}
-	return 0;
+    uint_fast64_t t;
+    cin >> t;
+    for (uint_fast64_t tNumber = 1u; tNumber < t + 1; ++tNumber)
+    {
+        if(solve() == -1)
+            return -1;
+    }
+    return 0;
 }
--- 2018/Quali/SavingTheUniverseAgain.cpp
+++ 2018/Quali/SavingTheUniverseAgain.cpp
@@ -9,96 +9,96 @@ using namespace std;
 class Sequence
 {
 public:
-	Sequence(string& s)
-	{
-		shots_.reserve(s.size());
-		shots_.push_back(0u);
-		for (char c : s)
-		{
-			if (c == 'S')
-			{
-				shots_.back()++;
-			}
-			else
-			{
-				shots_.push_back(0u);
-			}
-		}
-		clearEnd();
-	}
+    Sequence(string& s)
+    {
+        shots_.reserve(s.size());
+        shots_.push_back(0u);
+        for (char c : s)
+        {
+            if (c == 'S')
+            {
+                shots_.back()++;
+            }
+            else
+            {
+                shots_.push_back(0u);
+            }
+        }
+        clearEnd();
+    }
 
-	int reduceToDamage(uint_fast64_t targetDamage)
-	{
-		uint_fast64_t currentDamage = calculateCurrentDamage();
-		int numberOfSteps = 0;
-		while (currentDamage > targetDamage && shots_.size() > 1)
-		{
-			const uint_fast64_t highestSlot = shots_.size() - 1u;
-			const uint_fast64_t secondHighestSlot = highestSlot - 1u;
-			const uint_fast64_t dmgReduction =
-				calculateDmgForSlot(secondHighestSlot);
-			shots_[highestSlot]--;
-			shots_[secondHighestSlot]++;
-			clearEnd();
+    int reduceToDamage(uint_fast64_t targetDamage)
+    {
+        uint_fast64_t currentDamage = calculateCurrentDamage();
+        int numberOfSteps = 0;
+        while (currentDamage > targetDamage && shots_.size() > 1)
+        {
+            const uint_fast64_t highestSlot = shots_.size() - 1u;
+            const uint_fast64_t secondHighestSlot = highestSlot - 1u;
+            const uint_fast64_t dmgReduction =
+                calculateDmgForSlot(secondHighestSlot);
+            shots_[highestSlot]--;
+            shots_[secondHighestSlot]++;
+            clearEnd();
 
-			numberOfSteps++;
-			currentDamage -= dmgReduction;
-		}
+            numberOfSteps++;
+            currentDamage -= dmgReduction;
+        }
 
-		if (currentDamage <= targetDamage)
-			return numberOfSteps;
-		return -1;
-	}
-
-private:
-	uint_fast64_t calculateCurrentDamage() const
-	{
-		uint_fast64_t damage = 0u;
-		for (uint_fast64_t slot = 0u; slot < shots_.size(); ++slot)
-		{
-			damage += calculateDmgForSlot(slot) * shots_[slot];
-		}
-		return damage;
-	}
-
-	uint_fast64_t calculateDmgForSlot(uint_fast64_t slot) const
-	{
-		return pow(2.0, static_cast<double>(slot));
-	}
-
-	void clearEnd()
-	{
-		while (shots_.back() == 0u)
-		{
-			shots_.pop_back();
-		}
-	}
+        if (currentDamage <= targetDamage)
+            return numberOfSteps;
+        return -1;
+    }
 
 private:
-	vector<uint_fast64_t> shots_;
+    uint_fast64_t calculateCurrentDamage() const
+    {
+        uint_fast64_t damage = 0u;
+        for (uint_fast64_t slot = 0u; slot < shots_.size(); ++slot)
+        {
+            damage += calculateDmgForSlot(slot) * shots_[slot];
+        }
+        return damage;
+    }
+
+    uint_fast64_t calculateDmgForSlot(uint_fast64_t slot) const
+    {
+        return pow(2.0, static_cast<double>(slot));
+    }
+
+    void clearEnd()
+    {
+        while (shots_.back() == 0u)
+        {
+            shots_.pop_back();
+        }
+    }
+
+private:
+    vector<uint_fast64_t> shots_;
 };
 
 int main()
 {
-	cin.tie(nullptr);
+    cin.tie(nullptr);
     ios::sync_with_stdio(false);
 
-	uint_fast64_t t;
-	cin >> t;
-	for (uint_fast64_t tNumber = 1u; tNumber < t + 1; ++tNumber)
-	{
-		uint_fast64_t targetDmg;
-		cin >> targetDmg;
-		string s;
-		cin >> s;
-		Sequence shotSequence(s);
-		const int solution = shotSequence.reduceToDamage(targetDmg);
-		cout << "Case #" << tNumber << ": ";
-		if (solution == -1)
-			cout << "IMPOSSIBLE";
-		else
-			cout << solution;
-		cout << '\n';
-	}
-	return 0;
+    uint_fast64_t t;
+    cin >> t;
+    for (uint_fast64_t tNumber = 1u; tNumber < t + 1; ++tNumber)
+    {
+        uint_fast64_t targetDmg;
+        cin >> targetDmg;
+        string s;
+        cin >> s;
+        Sequence shotSequence(s);
+        const int solution = shotSequence.reduceToDamage(targetDmg);
+        cout << "Case #" << tNumber << ": ";
+        if (solution == -1)
+            cout << "IMPOSSIBLE";
+        else
+            cout << solution;
+        cout << '\n';
+    }
+    return 0;
 }
